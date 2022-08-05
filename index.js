a28d39b2b0629b033ab35815add0088a4d3a54f7
Adelgado4419@gmail.com <Adelgado4419@gmail.com>
2022-08-04 19:17:22 -0700

    finished steps 1 and 2

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
+
 // Your Code Here
